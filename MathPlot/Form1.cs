e1b8b467d21e6b667cebcdb1eded23db94677f52
kaneko <40829284+jr0nku@users.noreply.github.com>
2020-02-09 23:43:34 +0900

    1st release

--- MathPlot/Form1.cs
+++ MathPlot/Form1.cs
@@ -18,10 +18,14 @@ namespace MathPlot
         private const double Ymin = -2.0;
         private const double Ymax = 2.0;
 
-        private const double GX = 800.0 / (Xmax - Xmin);     /* width        */
-        private const double GY = -400.0 / (Ymax - Ymin);    /* height       */
-        private const int CX = 400;
-        private const int CY = 200;
+        private const int CX = 10;
+        private const int CY = 150;
+        private const int XPOS = 10;
+        private const int YPOS = 10;
+        private const int XWIDTH = 500;
+        private const int YHEIGHT = 300;
+        private const double GX = XWIDTH / (Xmax - Xmin);     /* width        */
+        private const double GY = -YHEIGHT / (Ymax - Ymin);    /* height       */
 
         /*===== initialize =====*/
         public Form1()
@@ -33,10 +37,29 @@ namespace MathPlot
         /*===== paint handler =====*/
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+
+            /*----- draw graph area -----*/
+            SolidBrush brushWhite = new SolidBrush(Color.White);
+            e.Graphics.FillRectangle(brushWhite, XPOS, YPOS, XWIDTH, YHEIGHT);
+            brushWhite.Dispose();
+
+            /*----- draw graph -----*/
+            Pen blackPen = new Pen(Color.Black);
+
             double a = double.Parse(textBoxA.Text);
-            for(double x = Xmin; x < Xmax; x += Xstep)
+            for (double x = Xmin; x <= Xmax - Xstep; x += Xstep)
             {
-                double y = Math.Sin(a * x);
+                double y0 = Math.Sin(a * x);
+                double y1 = Math.Sin(a * (x + Xstep));
+                //double y0 = 1.0 / (1.0 + x * x);
+                //double y1 = 1.0 / (1.0 + (x + Xstep) * (x + Xstep));
+
+                int gx0 = CX + (int)(x * GX);
+                int gy0 = CY + (int)(y0 * GY);
+                int gx1 = CX + (int)((x + Xstep) * GX);
+                int gy1 = CY + (int)(y1 * GY);
+                e.Graphics.DrawLine(blackPen, gx0, gy0, gx1, gy1);
+
             }
 
         }
@@ -44,7 +67,7 @@ namespace MathPlot
         /*===== Graph button click =====*/
         private void buttonGraph_Click(object sender, EventArgs e)
         {
-
+            Invalidate();
         }
     }
 }
